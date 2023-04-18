9fc54382d156d044f7bf86b2016577a41f6a6088
Ron Thomas <ronthomasgeorge@gmail.com>
2023-04-18 07:45:50 +0530

    Strings basics

--- strings.py
+++ strings.py
@@ -1,2 +1,18 @@
 a = "My Cars"
 print(a)
+
+x = "Morning Ride"
+print(x[5])
+
+for y in "Super Cars":
+    print(y)
+
+b ="Ride for a sunset"   
+print(len(b)) 
+
+c = "Good things are wild and free"
+print("free" in c)
+
+d = "What a ride"
+if "ride" in d:
+    print("Chill ride in a morning")
